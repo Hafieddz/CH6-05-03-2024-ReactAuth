ffdcd5f12d903bd3dc36504a30aece40d02857fc
Hafieddz <hasmy41@gmail.com>
2024-05-03 22:32:50 +0800

    push project to own repo

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { login } from "../../services/auth.services";
 import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
+  const [loginFailed, setLoginFailed] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+        window.location.href = "/product";
+      } else {
+        setLoginFailed(res.response.data);
+      }
+    });
+  };
+
   return (
-    <form className="max-w-sm mx-auto">
+    <form className="max-w-sm mx-auto" onSubmit={handleLogin}>
       <InputForm
         label="Username"
         name="username"
@@ -20,6 +42,9 @@ const FormLogin = () => {
       <Button type="submit" classname="bg-blue-600 w-full">
         Login
       </Button>
+      {loginFailed && (
+        <p className="text-red-500 text-center mt-3">{loginFailed}</p>
+      )}
     </form>
   );
 };
